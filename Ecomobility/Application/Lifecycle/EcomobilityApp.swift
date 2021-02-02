78026c844e35b985a4211a475f2673853912fe98
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-02-02 18:22:54 +0100

    Project structure created

---
+++ Ecomobility/Application/Lifecycle/EcomobilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcomobilityApp.swift
+//  Ecomobility
+//
+//  Created by Raúl Pera Pairó on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct EcomobilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
